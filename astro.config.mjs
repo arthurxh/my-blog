193cc223b07e44408827b5a3d597e62444485835
hu <1137247090@qq.com>
2026-07-04 15:50:49 +0800

    build(astro.config): update site domain and base path
    修改了站点配置为自定义域名https://www.asherxh.top，并且清空了base路径适配部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://arthurxh.github.io",
-	base: "/my-blog",
+	site: "https://www.asherxh.top",
+	base: "",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
